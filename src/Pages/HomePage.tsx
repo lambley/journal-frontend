acf92671ffb9828262daf696c0ce45031e3a7185
Aaron Lambley <aaron.lambley@gmail.com>
2023-07-27 10:17:18 +0100

    set list view to be default

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,19 +3,19 @@ import { PostList } from "../Components/PostList";
 import { useState } from "react";
 
 export const HomePage = () => {
-  const [showList, setShowList] = useState(false)
+  const [showCalendar, setShowCalendar] = useState(false)
 
-  const buttonText = showList ? "Show Calendar" : "Show List"
+  const buttonText = showCalendar ? "List View" : "Calendar View"
 
   const handleToggleClick = () => {
-    setShowList(!showList)
+    setShowCalendar(!showCalendar)
   }
 
   return (
     <div>
       <h1>Home Page</h1>
       <button className="btn btn-secondary" onClick={handleToggleClick}>{buttonText}</button>
-      {showList ? <PostList /> : <MyCalendar />}
+      {showCalendar ? <MyCalendar /> : <PostList />}
     </div>
   )
 }
