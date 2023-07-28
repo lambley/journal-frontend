914bb2437936a3bcc2e04e62826fb4a5b28f5c70
Aaron Lambley <aaron.lambley@gmail.com>
2023-07-28 16:00:51 +0100

    centre button group

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -14,44 +14,46 @@ export const HomePage = () => {
 
   return (
     <div className="home-page">
-      <ButtonGroup aria-label="Basic example">
-        <Button
-          variant="secondary"
-          onClick={() => {
-            handleViewClick("TodayView");
-          }}
-          active={activeView === "TodayView"}
-        >
-          Today
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            handleViewClick("PostList");
-          }}
-          active={activeView === "PostList"}
-        >
-          List
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            handleViewClick("MyCalendar");
-          }}
-          active={activeView === "MyCalendar"}
-        >
-          Calendar
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            handleViewClick("YearOverviewChart");
-          }}
-          active={activeView === "YearOverviewChart"}
-        >
-          Year
-        </Button>
-      </ButtonGroup>
+      <div className="navigation-buttons d-flex justify-content-center mt-3">
+        <ButtonGroup aria-label="Basic example">
+          <Button
+            variant="secondary"
+            onClick={() => {
+              handleViewClick("TodayView");
+            }}
+            active={activeView === "TodayView"}
+          >
+            Today
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              handleViewClick("PostList");
+            }}
+            active={activeView === "PostList"}
+          >
+            List
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              handleViewClick("MyCalendar");
+            }}
+            active={activeView === "MyCalendar"}
+          >
+            Calendar
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              handleViewClick("YearOverviewChart");
+            }}
+            active={activeView === "YearOverviewChart"}
+          >
+            Year
+          </Button>
+        </ButtonGroup>
+      </div>
       <Tab.Container activeKey={activeView}>
         <Tab.Content>
           <Tab.Pane eventKey="TodayView">
