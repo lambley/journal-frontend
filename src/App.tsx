451c5838b7c4f44a6e6f9b3540b3953cbf705a1a
Aaron Lambley <aaron.lambley@gmail.com>
2023-07-28 11:47:34 +0100

    update navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,27 @@ import { HomePage } from './Pages/HomePage';
 import { NotFoundPage } from './Pages/NotFoundPage';
 import { AboutPage } from './Pages/AboutPage';
 import { Container, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+
 function App() {
   return (
     <>
+      <Navbar className="navbar-custom" expand="lg">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/" className="nav-link">
+            <FontAwesomeIcon icon={faHouse} />
+          </Navbar.Brand>
+          <Navbar.Brand as={Link} to="/" className="nav-link">
+            <h1>Famous Quotes</h1>
+          </Navbar.Brand>
+          <Navbar.Brand as={Link} to="/about" className="nav-link">
+            About
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
       <Container>
-        <Navbar expand="lg" variant="light" bg="light">
-          <Container>
-            <Link className="nav-link" to="/">Home</Link>
-            <Link className="nav-link" to="/about">About </Link>
-          </Container>
-        </Navbar><br />
-
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
@@ -23,4 +33,5 @@ function App() {
     </>
   );
 }
+
 export default App;
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,39 +2,59 @@ import { MyCalendar } from "../Components/Calendar";
 import { PostList } from "../Components/PostList";
 import { YearOverviewChart } from "../Components/YearOverviewChart";
 import { useState } from "react";
+import { ButtonGroup, Button, Tab, Tabs } from "react-bootstrap";
 
 export const HomePage = () => {
-  const [showCalendar, setShowCalendar] = useState<Boolean>(false)
-  const [showPostList, setShowPostList] = useState<Boolean>(true)
-  const [showYearOverviewChart, setShowYearOverviewChart] = useState<Boolean>(false)
+  const [activeView, setActiveView] = useState("PostList");
 
   const handleViewClick = (view:string) => {
-    if (view === "PostList") {
-      setShowPostList(true)
-      setShowCalendar(false)
-      setShowYearOverviewChart(false)
-    } else if (view === "MyCalendar") {
-      setShowPostList(false)
-      setShowCalendar(true)
-      setShowYearOverviewChart(false)
-    } else if (view === "YearOverviewChart") {
-      setShowPostList(false)
-      setShowCalendar(false)
-      setShowYearOverviewChart(true)
-    }
-  }
+    setActiveView(view);
+  };
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <div className="btn-group" role="group" aria-label="Basic example">
-        <button className="btn btn-secondary" onClick={()=>{handleViewClick("PostList")}}>List</button>
-        <button className="btn btn-secondary" onClick={()=>{handleViewClick("MyCalendar")}}>Calendar</button>
-        <button className="btn btn-secondary" onClick={()=>{handleViewClick("YearOverviewChart")}}>Year</button>
-      </div>
-      {showPostList && <PostList />}
-      {showCalendar && <MyCalendar />}
-      {showYearOverviewChart && <YearOverviewChart />}
+    <div className="home-page">
+      <ButtonGroup aria-label="Basic example">
+        <Button
+          variant="secondary"
+          onClick={() => {
+            handleViewClick("PostList");
+          }}
+          active={activeView === "PostList"}
+        >
+          List
+        </Button>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            handleViewClick("MyCalendar");
+          }}
+          active={activeView === "MyCalendar"}
+        >
+          Calendar
+        </Button>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            handleViewClick("YearOverviewChart");
+          }}
+          active={activeView === "YearOverviewChart"}
+        >
+          Year
+        </Button>
+      </ButtonGroup>
+      <Tab.Container activeKey={activeView}>
+        <Tab.Content>
+          <Tab.Pane eventKey="PostList">
+            <PostList />
+          </Tab.Pane>
+          <Tab.Pane eventKey="MyCalendar">
+            <MyCalendar />
+          </Tab.Pane>
+          <Tab.Pane eventKey="YearOverviewChart">
+            <YearOverviewChart />
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
     </div>
-  )
-}
+  );
+};
