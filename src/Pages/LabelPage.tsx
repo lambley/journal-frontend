cc808b31b93951a2e110028285ddb8e093e15568
Aaron Lambley <aaron.lambley@gmail.com>
2023-07-31 15:51:28 +0100

    render posts by label in LabelPage

--- src/Pages/LabelPage.tsx
+++ src/Pages/LabelPage.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { Post } from '../Components/Post'
+import { IPost } from '../types/data'
+import { axiosInstance } from '../Api/Api.js'
+import { Row, Col } from 'react-bootstrap'
 
 export const LabelPage:React.FC = () => {
-  const { label } = useParams<{ label:string }>()
+  const { label = '' } = useParams<{ label?:string }>();
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    getPosts(label)
+  }, [])
+
+  const getPosts = async (label: string) => {
+    const response = await axiosInstance.get("/api/v1/posts/label=" + label);
+    setPosts(response.data);
+  }
+
   return (
     <div>
-      <h1><span className={`label-page-heading post-label-${label}`}>#{label}</span> Quotes</h1>
+      <h1 className='text-center'> 
+        <span className={`label-page-heading post-label-${label}`}>#{label}</span> Quotes
+      </h1>
+      <Row xs={1} md={3} className="g-4 mt-3">
+        {posts.map((post: IPost) => (
+          <Col key={post.id}>
+            <Post {...post} />
+          </Col>
+        ))}
+      </Row>
     </div>
   )
 }
