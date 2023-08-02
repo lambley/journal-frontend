0f30f87aec507a5fb3ef4d95cd82bd2f0af4887f
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-02 22:53:25 +0100

    add login and logout to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 import { HomePage } from './Pages/HomePage';
 import { NotFoundPage } from './Pages/NotFoundPage';
-import { AboutPage } from './Pages/AboutPage';
+// import { AboutPage } from './Pages/AboutPage';
 import { LabelPage } from './Pages/LabelPage';
 import { LoginPage} from './Pages/LoginPage';
 import { Container, Navbar } from 'react-bootstrap';
@@ -9,6 +10,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faCommentDots } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    setLoggedInUser();
+  }, []);
+
+  useEffect(() => {
+    setLoggedInUser();
+  }, [username]);
+
+  const setLoggedInUser = () => {
+    const jwtToken = localStorage.getItem('jwtToken');
+    const decodedToken = jwtToken ? JSON.parse(atob(jwtToken.split('.')[1])) : null;
+    const loggedInUser = decodedToken ? decodedToken.username : null;
+    setUsername(loggedInUser);
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('jwtToken');
+    setUsername(null);
+  };
+
   return (
     <>
       <Navbar className="sticky-top navbar-custom" expand="lg">
@@ -17,14 +40,37 @@ function App() {
             <FontAwesomeIcon icon={faHouse} />
           </Navbar.Brand>
           <Navbar.Brand className="navbar-title ">
-            <h1>WiseWords <FontAwesomeIcon icon={faCommentDots} /></h1>
+            <h1 className='text-center'>WiseWords <FontAwesomeIcon icon={faCommentDots} /></h1>
           </Navbar.Brand>
-          <Navbar.Brand as={Link} to="/login" className="nav-link">
-            Login
-          </Navbar.Brand>
-          <Navbar.Brand as={Link} to="/about" className="nav-link">
-            About
-          </Navbar.Brand>
+          <div className="ml-auto">
+            { username
+            ? (
+                <div>
+                  <Navbar.Brand className="nav-link">
+                    Hello, {username.charAt(0).toUpperCase() + username.slice(1)}!
+                  </Navbar.Brand>
+                  <Navbar.Brand
+                    as={Link} to=""
+                    className="nav-link"
+                    onClick={handleLogout}
+                    aria-label='logout'
+                  >
+                    Logout
+                  </Navbar.Brand>
+                </div>
+              )
+            : (
+                <Navbar.Brand
+                  as={Link}
+                  to="/login"
+                  className="nav-link"
+                  aria-label='login'
+                >
+                  Login
+                </Navbar.Brand>
+              )
+            }
+          </div>
         </Container>
       </Navbar>
       <br />
@@ -32,7 +78,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/about" element={<AboutPage />} />
+          {/* <Route path="/about" element={<AboutPage />} /> */}
           <Route path="label/:label" element={<LabelPage/>} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
