7716205bda02387d318d50c91ce66a05a26b52eb
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-02 22:17:31 +0100

    add content headers

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -3,5 +3,9 @@ import axios from "axios";
 const url = process.env.REACT_APP_API_URL || "http://localhost:3000"
 
 export const axiosInstance = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
   baseURL: url
 })
