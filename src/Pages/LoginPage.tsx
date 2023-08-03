c0e6e3f88149ed8202f223e5b733360f82480c3c
Aaron Lambley <aaron.lambley@gmail.com>
2023-08-03 14:31:16 +0100

    add basic login form style

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import { axiosInstance } from '../Api/Api';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 interface LoginPageProps {
   onLoginStatusChange: () => void;
 }
 
-export const LoginPage: React.FC<LoginPageProps> = ({onLoginStatusChange}) => {
+export const LoginPage: React.FC<LoginPageProps> = ({ onLoginStatusChange }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -19,8 +20,8 @@ export const LoginPage: React.FC<LoginPageProps> = ({onLoginStatusChange}) => {
       const response = await axiosInstance.post('/login', {
         user: {
           email,
-          password
-        }
+          password,
+        },
       });
 
       const { user, token } = response.data;
@@ -34,31 +35,43 @@ export const LoginPage: React.FC<LoginPageProps> = ({onLoginStatusChange}) => {
       } else {
         console.log('Login failed:', Error);
       }
-
     } catch (error) {
       console.error('Login failed:', error);
-
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col xs={12} md={6}>
+          <h2 className="mb-4">Login</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password" className='mt-3'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className='mt-3'>
+              Login
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
